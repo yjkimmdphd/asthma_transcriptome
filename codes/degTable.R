6ceb8061f8610a2095770840ae41bb27a421041f
yjkimmdphd <29801275+yjkimmdphd@users.noreply.github.com>
2023-09-29 12:41:52 -0400

    updated degtable

--- codes/degTable.R
+++ codes/degTable.R
@@ -1,41 +1,43 @@
-##############
+########################
 ## exploring DEG results
 ########################
 library(tidyverse)
-fp<-file.path(getwd(),"output/DEG_2023-09-26")
-if(file.exists(fp)){
+
+# if individual DEG tables of each DEG results are available,
+# read them and save them as individual data.frame 
+# then summarize the results in res.table
+if(file.exists(file.path(getwd(),"output/DEG_2023-09-26"))){
   flist<-list.files(fp)[1:36]
-}
-fl<-sapply(flist,function(d)str_split(d,"DEG_res"))%>%unlist%>%matrix(ncol=2, byrow=TRUE)
-fl<-fl[,2]%>%str_split("_")%>%unlist%>%matrix(ncol=4,byrow=TRUE)
-fl<-fl[,1]%>%as.numeric%>%na.omit%>%as.numeric
-
-
-for(i in fl){
-  assign(paste0("res",i),read.csv(file.path(fp,flist[i]))%>%mutate(results=paste0("res",i))%>%rename(X="genes"))
+  fl<-sapply(flist,function(d)str_split(d,"DEG_res"))%>%unlist%>%matrix(ncol=2, byrow=TRUE)
+  fl<-fl[,2]%>%str_split("_")%>%unlist%>%matrix(ncol=4,byrow=TRUE)
+  fl<-fl[,1]%>%as.numeric%>%na.omit%>%as.numeric
+  for(i in fl){
+    assign(paste0("res",i),read.csv(file.path(fp,flist[i]))%>%mutate(results=paste0("res",i))%>%rename(X="genes"))
+    
+    
+  }
+  # summarize number of significant genes in the 'res.table'
+  cont.var<-grep("zero",res.table$fluid_cell, invert=TRUE)
+  for(i in cont.var){
+    res.table[i,"sig.genes"]<-get(paste0("res",i))%>%filter(padj<0.05)%>%nrow
+    res.table[i,"results"]<-paste0("res",i)
+  }
+  print(res.table)
   
-         }
-
-cont.var<-grep("zero",res.table$fluid_cell, invert=TRUE)
-
-# summarize number of significant genes in the 'res.table'
-
-for(i in cont.var){
-  res.table[i,"sig.genes"]<-get(paste0("res",i))%>%filter(padj<0.05)%>%nrow
-  res.table[i,"results"]<-paste0("res",i)
+  # write sig.table csv
+  write.csv(res.table,file.path(getwd(),"output",paste0("res.table_",Sys.Date(),".csv")),row.names = FALSE)
+  ##############################
+  # make list of significant DEG 
+  ##############################
+  
+  dt<-rbindlist((lapply(res.list,get)))
+  sig.deg<-dt%>%filter(padj<0.05)%>%group_by(results)
+  write.csv(
+    sig.deg,file.path(getwd(),"output",paste0("sig_deg_",Sys.Date(),".csv")),row.names=FALSE
+  )
+} else{
+  sig.deg<-file.path(getwd(),"output/sig_deg_2023-09-28.csv")%>%read.csv
+  res.table<-file.path(getwd(),"output/res.table_2023-09-28.csv")%>%read.csv%>%na.omit
 }
-print(res.table)
 
-# write sig.table csv
-write.csv(res.table,file.path(getwd(),"output",paste0("res.table_",Sys.Date(),".csv")),row.names = FALSE)
-
-##############################
-# make list of significant DEG 
-##############################
-
-dt<-rbindlist((lapply(res.list,get)))
-sig.deg<-dt%>%filter(padj<0.05)%>%group_by(results)
-write.csv(
-  sig.deg,file.path(getwd(),"output",paste0("sig_deg_",Sys.Date(),".csv")),row.names=FALSE
-)
-table(sig.deg$genes)%>%as.data.frame()%>%arrange(desc(Freq))
+table(sig.deg$genes)%>%as.data.frame()%>%arrange(desc(Freq))%>%print
